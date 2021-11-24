7123bef11b24d46ed0a0ef19d7b76753975c7627
Yoon <respect5716@gmail.com>
2021-11-24 13:29:26 +0900

    UPDATE namuwiki

--- prepare_namuwiki.py
+++ prepare_namuwiki.py
@@ -2,24 +2,19 @@ import json
 from tqdm.auto import tqdm
 from namuwiki.extractor import extract_text
 
+from utils import read_json
+
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--namuwiki_dump_path', type=str)
+parser.add_argument('--input_file', type=str)
+parser.add_argument('--output_file', type=str, default='namuwiki.txt')
 parser.add_argument('--min_document_length', type=int, default=20)
 args = parser.parse_args()
 
 
-def load_namuwiki_dump(namuwiki_dump_path):
-    with open(namuwiki_dump_path, 'r', encoding='utf-8') as f:
-        namu_wiki = json.load(f)
-    
-    print(f'Loaded {len(namu_wiki)} documents')
-    return namu_wiki
-
-
 def main(args):
-    namu_wiki = load_namuwiki_dump(args.namuwiki_dump_path)
-    fs = open('namuwiki.txt', 'a') 
+    namu_wiki = read_json(args.input_file)
+    fs = open(args.output_file, 'a') 
     for idx, document in enumerate(tqdm(namu_wiki)):
         text = extract_text(document['text'])
         text = text.replace('\n', '  ')
